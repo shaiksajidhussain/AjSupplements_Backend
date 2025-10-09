d5d00bf5b50c0e4033eb4bd045472d9de0ac71ab
shaiksajidhussain <sanjusazid0@gmail.com>
2025-10-09 21:59:16 +0530

    Added the cors

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://www.ajsupplements.com'
+  ],
   credentials: true
 }));
 app.use(express.json());
